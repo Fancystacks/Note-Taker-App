7a46099ca0ea0923178bb6a8246eb24e17f76259
Leah Migacz <lmigacz@msudenver.edu>
2020-01-27 08:41:50 -0700

    fixed functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var newNote = require("../db/db.json");
+const uuidv4 = require("uuid/v4");
 
 module.exports = function (app) {
 
@@ -7,6 +8,17 @@ module.exports = function (app) {
     });
 
     app.get("/api/notes/:note", function (req, res) {
+        app.post("/api/notes", (req, res) => {
+            const newNote = req.body;
+            const file = path.join(__dirname, "../db/db.json");
+            newNote.id = uuidv4();
+            notesArray.push(newNote);
+            fs.writeFile(file, JSON.stringify(notesArray, null, 4), err => {
+                if (err) throw err;
+                console.log("New note has been saved.");
+            });
+            res.send(newNote);
+        });
         var selected = req.params.note;
         newNote(selected)
         res.json(true);
